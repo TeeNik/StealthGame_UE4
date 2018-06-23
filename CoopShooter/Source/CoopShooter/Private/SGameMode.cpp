f3c9a7ce1dc35063eef27042e863413f80130dfb
TeeNik <teenik3315@gmail.com>
2018-06-23 19:39:01 +0300

    waves

--- CoopShooter/Source/CoopShooter/Private/SGameMode.cpp
+++ CoopShooter/Source/CoopShooter/Private/SGameMode.cpp
@@ -1,7 +1,46 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "SGameMode.h"
+#include "TimerManager.h"
 
 
+
+ASGameMode::ASGameMode()
+{
+	TimeBetweenWaves = 2;
+}
+
+void ASGameMode::StartPlay()
+{
+	Super::StartPlay();
+	PrepareForNextWave();
+}
+
+void ASGameMode::StartWave()
+{
+	++WaveCount;
+	NumOfBotsToSpawn = 2 * WaveCount;
+	GetWorldTimerManager().SetTimer(BotSpawnTimer, this, &ASGameMode::SpawnNewBot, 1, true, 0);
+}
+
+void ASGameMode::EndWave()
+{
+	GetWorldTimerManager().ClearTimer(BotSpawnTimer);
+	PrepareForNextWave();
+}
+
+void ASGameMode::PrepareForNextWave()
+{
+	FTimerHandle NextWaveTimer;
+	GetWorldTimerManager().SetTimer(NextWaveTimer, this, &ASGameMode::StartWave, TimeBetweenWaves, false);
+}
+
+void ASGameMode::SpawnBotTimerElapsed()
+{
+	SpawnNewBot();
+	--NumOfBotsToSpawn;
+	if (NumOfBotsToSpawn == 0)
+		EndWave();
+}
 
 
--- CoopShooter/Source/CoopShooter/Public/SGameMode.h
+++ CoopShooter/Source/CoopShooter/Public/SGameMode.h
@@ -14,7 +14,27 @@ class COOPSHOOTER_API ASGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 	
+protected:
+
+	FTimerHandle BotSpawnTimer;
+	int32 NumOfBotsToSpawn;
+	int32 WaveCount;
+
+	UPROPERTY(EditDefaultsOnly, Category="GameMode")
+	int32 TimeBetweenWaves;
+
+	UFUNCTION(BlueprintImplementableEvent, Category="GameMode")
+	void SpawnNewBot();
 	
+	void StartWave();
+	void EndWave();
+	void PrepareForNextWave();
+	void SpawnBotTimerElapsed();
+
+public:
+
+	ASGameMode();
+	virtual void StartPlay() override;
 	
 	
 };
